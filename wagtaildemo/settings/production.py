6449bb7f3b7555f655c802c3d86ffc8cb34982b6
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-05-24 14:08:54 +1000

    Drop Redis configuration and use DATABASE_URL to define database to use for production settings.

--- wagtaildemo/settings/production.py
+++ wagtaildemo/settings/production.py
@@ -8,46 +8,11 @@ try:
 except ImportError:
     pass
 
-
 import os
-
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+import dj_database_url
 
 ALLOWED_HOSTS = ['*']
 
-if os.environ.get('OPENSHIFT_MYSQL_DB_HOST'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ.get('OPENSHIFT_MYSQL_DB_NAME'),
-            'USER': os.environ.get('OPENSHIFT_MYSQL_DB_USERNAME'),
-            'PASSWORD': os.environ.get('OPENSHIFT_MYSQL_DB_PASSWORD'),
-            'HOST': os.environ.get('OPENSHIFT_MYSQL_DB_HOST'),
-            'PORT': os.environ.get('OPENSHIFT_MYSQL_DB_PORT')
-        }
-    }
-elif os.environ.get('OPENSHIFT_POSTGRESQL_DB_HOST'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('OPENSHIFT_POSTGRESQL_DB_NAME'),
-            'USER': os.environ.get('OPENSHIFT_POSTGRESQL_DB_USERNAME'),
-            'PASSWORD': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PASSWORD'),
-            'HOST': os.environ.get('OPENSHIFT_POSTGRESQL_DB_HOST'),
-            'PORT': os.environ.get('OPENSHIFT_POSTGRESQL_DB_PORT')
-        }
-    }
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-if os.environ.get('OPENSHIFT_REDIS_HOST'):
-    CACHES = {
-        'default': {
-            'BACKEND': 'django_redis.cache.RedisCache',
-            'LOCATION': 'redis://%s:%s/%s' % (
-                    os.environ.get('OPENSHIFT_REDIS_HOST'),
-                    os.environ.get('OPENSHIFT_REDIS_PORT'),
-                    os.environ.get('OPENSHIFT_REDIS_DB_NUMBER', '0')),
-            'OPTIONS': {
-                'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            }
-        }
-    }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)

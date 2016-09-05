5763b90a39c76e284c96b48c227d7041321cd091
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-09-05 21:59:59 +1000

    Added extended metrics for datadog.

--- wagtaildemo/metrics.py
+++ wagtaildemo/metrics.py
@@ -8,13 +8,29 @@ from datadog import statsd
 def event_handler(name, **kwargs):
     if name == 'request_finished':
         statsd.increment('mod_wsgi.request.count')
-        statsd.histogram('mod_wsgi.request.application_time', kwargs.get('application_time'))
-        statsd.gauge('mod_wsgi.request.cpu_user_time', kwargs.get('cpu_user_time'))
-        statsd.gauge('mod_wsgi.request.cpu_system_time', kwargs.get('cpu_system_time'))
+
+        application_time = kwargs.get('application_time')
+
+        statsd.histogram('mod_wsgi.request.application_time', application_time)
+
+        statsd.gauge('mod_wsgi.request.input_reads', kwargs.get('input_reads'))
+        statsd.gauge('mod_wsgi.request.input_length', kwargs.get('input_length'))
+        statsd.gauge('mod_wsgi.request.input_time', kwargs.get('input_time'))
+
+        statsd.gauge('mod_wsgi.request.output_writes', kwargs.get('output_writes'))
+        statsd.gauge('mod_wsgi.request.output_length', kwargs.get('output_length'))
+        statsd.gauge('mod_wsgi.request.output_time', kwargs.get('output_time'))
+
+        cpu_user_time = kwargs.get('cpu_user_time')
+        cpu_system_time = kwargs.get('cpu_system_time')
+
+        statsd.gauge('mod_wsgi.request.cpu_user_time', cpu_user_time)
+        statsd.gauge('mod_wsgi.request.cpu_system_time', cpu_system_time)
+
+        if cpu_user_time is not None and application_time:
+            cpu_burn = (cpu_user_time + cpu_system_time) / application_time
+            statsd.gauge('mod_wsgi.request.cpu_burn', cpu_burn)
 
 def initialize():
     if event_handler not in mod_wsgi.event_callbacks:
-        api_key = os.environ.get('DATADOG_API_KEY')
-
-        datadog.initialize(api_key=api_key)
         mod_wsgi.subscribe_events(event_handler)

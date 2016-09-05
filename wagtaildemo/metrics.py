0253d91d4ab851ea4528b686d74af739d44f7a5b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-09-05 21:52:57 +1000

    Incorrect value for response time.

--- wagtaildemo/metrics.py
+++ wagtaildemo/metrics.py
@@ -8,10 +8,9 @@ from datadog import statsd
 def event_handler(name, **kwargs):
     if name == 'request_finished':
         statsd.increment('mod_wsgi.request.count')
-        statsd.histogram('mod_wsgi.request.response_time', kwargs.get('response_time'))
+        statsd.histogram('mod_wsgi.request.application_time', kwargs.get('application_time'))
         statsd.gauge('mod_wsgi.request.cpu_user_time', kwargs.get('cpu_user_time'))
         statsd.gauge('mod_wsgi.request.cpu_system_time', kwargs.get('cpu_system_time'))
-        statsd.gauge('mod_wsgi.request.output_length', kwargs.get('output_length'))
 
 def initialize():
     if event_handler not in mod_wsgi.event_callbacks:
